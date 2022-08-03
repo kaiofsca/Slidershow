9c1cfa2933509f2d78abcb18672c7938cd91c5f0
Kaio Fonseca <mkaiofonseca@gmail.com>
2022-08-03 15:48:48 -0300

    ainda parte inicial

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const images = [
+    {'id': '1', 'url':'./img/chrono.jpg'},
+    {'id': '2', 'url':'./img/inuyasha.jpg'},
+    {'id': '3', 'url':'./img/tenchi.jpg'},
+    {'id': '4', 'url':'./img/tenjhotenge.jpg'},
+    {'id': '5', 'url':'./img/yuyuhakusho.jpg'},
+    {'id': '6', 'url':'./img/ippo.png'}
+]
+
+loadImages(images)
